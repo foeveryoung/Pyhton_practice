7fd87f9f6e853d3ad96ef31ca767aa5f8dea6538
foeveryoung <862515154@qq.com>
2018-01-22 20:52:11 +0800

    Update Iterator.py

--- Iterator.py
+++ Iterator.py
@@ -7,4 +7,32 @@ a=[11,22,33]
 a=iter(a)   #使用iter方法将list转为了一个迭代器
 type(a)     #此时a为一个迭代器，可以用next（a）对a循环
 
+# -*-coding:utf-8 -*-
+def Fib():
+    a,b=0,1
+    print("开始")
+    for i in range(2):
+        print('运行至'+str(i+1)+'次')
+        yield b
+        print("结束")
 
+#用for循环调用生成器
+a=Fib()
+for item in a:
+    print(item)
+#与next等价
+# a=Fib()
+# a.__next__()
+# a.__next__()
+# a.__next__()
+
+#<><><><><><><><><><><><><><>
+def func():
+    i=0
+    while i<5:
+        item=yield i
+        print(item)
+        i+=1
+f=func()
+f.send('Python')  #send在返回值出传入值
+f.send('小明')
